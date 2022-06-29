0af3685a02056755e615e67d2be2c8f8bbe07890
basicallygit <niallgrim1@gmail.com>
2022-06-29 10:04:26 +0100

    touch defaults to modifying access time

--- touch/src/main.rs
+++ touch/src/main.rs
@@ -9,18 +9,9 @@ fn main() {
         println!("Usage: touch [filename]");    
     } else {
         for i in 1..argv.len() {
-<<<<<<< HEAD
             //if file does not already exist, create it, if not the timestamp is updated
             if !Path::new(&argv[i]).exists() {
                 File::create(&argv[i]).unwrap();
-=======
-            //check if file exists
-            if Path::new(&argv[i]).exists() {
-                println!("File with name {} already exists", &argv[i]);
-                print!("Continue to overwrite? (y/N) "); // Touch by default change acess time stamp if file already exist
-            
-                }
->>>>>>> 5acc7bad2c9a068c42b1b70e430b45b22e8d42d1
             }
             else {
                 //write a single space to the file to update the modified timestamp
