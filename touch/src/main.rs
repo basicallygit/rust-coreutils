28977ca83b2a0736057c7aa16a62c598e08aa288
basicallygit <niallgrim1@gmail.com>
2022-06-29 09:57:14 +0100

    touch defaults to modifying access time

--- touch/src/main.rs
+++ touch/src/main.rs
@@ -1,27 +1,27 @@
-use std::io::Write;
 use std::path::Path;
-use std::fs::File;
-use std::{env, io};
+use std::fs::{File, OpenOptions};
+use std::env;
+use std::io::Write;
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
     if argv.len() == 1 {
-        println!("Usage: touch [filename]");
+        println!("Usage: touch [filename]");    
     } else {
         for i in 1..argv.len() {
-            //check if file exists
-            if Path::new(&argv[i]).exists() {
-                println!("File with name {} already exists", &argv[i]);
-                print!("Continue to overwrite? (y/N) "); // no by default
-                io::stdout().flush().unwrap();
-                let mut input = String::new();
-                io::stdin().read_line(&mut input).unwrap();
-                if input.trim().to_lowercase().starts_with("y") {
-                    File::create(&argv[i]).unwrap();
-                }
+            //if file does not already exist, create it, if not the timestamp is updated
+            if !Path::new(&argv[i]).exists() {
+                File::create(&argv[i]).unwrap();
             }
             else {
-                File::create(&argv[i]).unwrap();
+                //write a single space to the file to update the modified timestamp
+                //TODO: this is a quick fix, need to find a better way to modify it
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .open(&argv[i])
+                    .unwrap();
+                write!(file, " ").unwrap();
             }
         }
     }
